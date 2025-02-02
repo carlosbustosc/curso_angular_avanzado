1625e0c65735e51c2e7aa7e9cc122835e28ed604
carlos alberto bustos <proyectosangular09@gmail.com>
2025-02-02 06:10:33 -0500

    diseño del curso angular avanzado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboradComponent } from './pages/dashborad/dashborad.component';
+import { LoginComponent } from './registros/login/login.component';
+import { RegistroComponent } from './registros/registro/registro.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { Nopage404Component } from './pages/nopage404/nopage404.component';
+
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+
+  { path:"", 
+    component:PagesComponent, //contiene el template
+    children:[
+
+      //rutas hijas
+      { path: "dashborard", component: DashboradComponent },
+      { path: "progress",  component: ProgressComponent  },
+      { path: "grafica1",   component: Grafica1Component },
+      { path: "", redirectTo:"dashborard", pathMatch:"full"}
+    
+    ]
+
+   },
+
+   
+  //------rutas con otro diseño-----//
+  { path:"registro",   component: RegistroComponent },
+  { path: "login",      component: LoginComponent },
+  
+  
+  { path: "**", component:Nopage404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//------------RUTAS----------//
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './registros/login/login.component';
+import { RegistroComponent } from './registros/registro/registro.component';
+import { Nopage404Component } from './pages/nopage404/nopage404.component';
+import { DashboradComponent } from './pages/dashborad/dashborad.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    Nopage404Component,
+    DashboradComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
